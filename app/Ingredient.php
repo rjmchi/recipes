1dc8eed87554dbbf934630003bd55ca2acca6f6c
rjmchi <robert@rjmprogramming.com>
2020-02-15 10:59:30 -0600

    Database models, migrations, and controllers

---
+++ app/Ingredient.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model
+{
+	public function recipe() {
+		return $this->belongsTo('App\Recipe');
+	}
+}
---
+++ app/Recipe.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+	public function ingredients() {
+        return $this->hasMany('App\Ingredient');	
+    }
+}
