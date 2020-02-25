278fe348c9045ddbf74fddc17cb4ab0ddc9fd2be
rjmchi <robert@rjmprogramming.com>
2020-02-25 17:10:20 -0600

    new component for adjustable value

---
+++ resources/js/components/AdjustableValueComponent.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <p>{{name}}: {{value}}
+        <button @click="increase()">+</button>
+        <button @click="decrease()">-</button>
+    </p> 
+</div>
+</template>
+<script>
+export default {
+    props: ['name', 'initValue', 'idx'],
+    data() {
+        return {
+            value: this.initValue,
+        }
+    },
+    methods: {
+        increase() {
+            this.value++;
+            this.$emit('update-value', {name: this.name, value:this.value, idx:this.idx});
+        },
+        decrease() {
+            this.value--;
+            this.$emit('update-value', {name: this.name, value:this.value, idx:this.idx});
+        }
+    },
+}
+</script>
+<style scoped>
+    p {
+        margin:2px;
+    }
+</style>
